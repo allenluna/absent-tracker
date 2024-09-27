3e1dd1d7187096d38036086059307b514de2bbb5
allen-luna <Mark.Luna@vxi.com>
2024-09-28 06:43:32 +0800

    done na talaga

--- JAM_AGENT_NOTIFICATION_TRACKER/wsgi.py
+++ JAM_AGENT_NOTIFICATION_TRACKER/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JAM_AGENT_NOTIFICATION_TRACKER.settings')
 
 application = get_wsgi_application()
+
+app = application
